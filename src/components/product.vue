f3fb44edb10329e14a5ff6fc548d3155c6bc6dc4
a183cute33 <a183cute33@gmail.com>
2018-06-25 13:46:49 +0800

    modify layout

--- src/components/product.vue
+++ src/components/product.vue
@@ -214,89 +214,88 @@
                 </tbody>
             </table>
         </div>
-    <div id="box">
-        <div class="content">
-            <div class="title">
-                ADD NEW PRODUCT
-                <i class="fas fa-times" @click="close"></i>
+        <div id="box">
+            <div class="content">
+                <div class="title">
+                    ADD NEW PRODUCT
+                    <i class="fas fa-times" @click="close"></i>
+                </div>
+                <div class="main">
+                    <div class="left">
+                        <div class="fas fa-upload upload">
+                            <b>Drag an image or click here to upload…</b>
+                            <div class="imgs">
+                                <div class="_ic"><img :src="require(`@/assets/logo.png`)"></div>
+                                <div class="_ic"><img :src="require(`@/assets/logo.png`)"></div>
+                                <div class="_ic"><img :src="require(`@/assets/logo.png`)"></div>
+                                <div class="_ic"><img :src="require(`@/assets/logo.png`)"></div>
+                                <div class="_ic"><img :src="require(`@/assets/logo.png`)"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="right">
+                        <b>Product Discription</b>
+                        <input value="Vestibulum nec">
+                        <textarea name="" id="" cols="30" rows="10">Curabitur lobortis id lorem id bibendum. Ut id consectetur magna. Quisque volutpat augue enim, pulvinar lobortis nibh lacinia at. Vestibulum nec erat ut mi sollicitudin porttitor id sit amet risus. Nam tempus vel odio vitae aliquam. </textarea>
+                        <b>Price</b>
+                        <div class="n2">
+                            <div class="set">
+                                <span>Original</span>
+                                <input value="$3,200">
+                            </div>
+                            <div class="set">
+                                <span>Discounts</span>
+                                <input value="$2,800">
+                            </div>
+                        </div>
+                        <b>Specification</b>
+                        <div class="n3">
+                            <div class="set">
+                                <span>Size</span>
+                                <select>
+                                    <option value="L">L</option>
+                                    <option value="M">M</option>
+                                    <option value="S">S</option>
+                                </select>
+                            </div>
+                            <div class="set">
+                                <span>Gray</span>
+                                <input value="Gray">
+                            </div>
+                            <div class="set">
+                                <span>Inventory</span>
+                                <input value="$15">
+                            </div>
+                        </div>
+                        <a class="add">ADD NEW SPECIFICATION<i class="fas fa-plus"></i></a>
+                    </div>
+                    <div class="bottom">
+                        <button>PUBLISH</button>
+                        <a href="">SAVE DRAFT</a>
+                    </div>
+                </div>
             </div>
-            <div class="main">
-                <div class="left">
-                    <div class="fas fa-upload upload">
-                        <b>Drag an image or click here to upload…</b>
-                        <div class="imgs">
-                            <div class="_ic"><img :src="require(`@/assets/logo.png`)"></div>
-                            <div class="_ic"><img :src="require(`@/assets/logo.png`)"></div>
-                            <div class="_ic"><img :src="require(`@/assets/logo.png`)"></div>
-                            <div class="_ic"><img :src="require(`@/assets/logo.png`)"></div>
-                            <div class="_ic"><img :src="require(`@/assets/logo.png`)"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="right">
-                    <b>Product Discription</b>
-                    <input value="Vestibulum nec">
-                    <textarea name="" id="" cols="30" rows="10">Curabitur lobortis id lorem id bibendum. Ut id consectetur magna. Quisque volutpat augue enim, pulvinar lobortis nibh lacinia at. Vestibulum nec erat ut mi sollicitudin porttitor id sit amet risus. Nam tempus vel odio vitae aliquam. </textarea>
-                    <b>Price</b>
-                    <div class="n2">
-                        <div class="set">
-                            <span>Original</span>
-                            <input value="$3,200">
-                        </div>
-                        <div class="set">
-                            <span>Discounts</span>
-                            <input value="$2,800">
-                        </div>
-                    </div>
-                    <b>Specification</b>
-                    <div class="n3">
-                         <div class="set">
-                            <span>Size</span>
-                            <select>
-                                <option value="L">L</option>
-                                <option value="M">M</option>
-                                <option value="S">S</option>
-                            </select>
-                        </div>
-                        <div class="set">
-                            <span>Gray</span>
-                            <input value="Gray">
-                        </div>
-                          <div class="set">
-                            <span>Inventory</span>
-                            <input value="$15">
-                        </div>
-                    </div>
-                    <a class="add">ADD NEW SPECIFICATION<i class="fas fa-plus"></i></a>
-                </div>
-                <div class="bottom">
-                    <button>PUBLISH</button>
-                    <a href="">SAVE DRAFT</a>
-                </div>
-            </div>
+            <div class="cover"></div>
         </div>
-        <div class="cover"></div>
-    </div>
-
     </div>
 </template>
 <script>
-$(function() {
-  var $box = $("#box");
-  $box.find(".add").click(function() {
-    var $tmp = $(this)
-      .prev()
-      .clone();
-    $tmp.find("input").val("");
-    $tmp
-      .find("select option")
-      .first()
-      .prop("selected");
-    $tmp.insertBefore($(this));
-  });
-});
 export default {
   name: "Product",
+  mounted() {
+    var $box = $("#box");
+    $box.find(".add").click(function() {
+      var $tmp = $(this)
+        .prev()
+        .clone();
+      $tmp.find("input").val("");
+      $tmp
+        .find("select option")
+        .first()
+        .prop("selected");
+      $tmp.insertBefore($(this));
+    });
+  },
   methods: {
     add() {
       var $box = $("#box");
