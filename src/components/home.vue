4a8d24801035b97a51a1ee7b4d6de255469c4ff3
a183cute33 <a183cute33@gmail.com>
2018-06-25 10:52:02 +0800

    modify layout

--- src/components/home.vue
+++ src/components/home.vue
@@ -144,44 +144,45 @@
           </div>
         </div>
       </div>
-       </div>
+    </div>
   </div>
 </template>
 
 <script>
-// import Image from './assets/logo.png';
-google.charts.load("current", { packages: ["corechart"] });
-google.charts.setOnLoadCallback(drawChart);
-
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-    ["Year", "Sales", "Expenses"],
-    ["2004", 1000, 400],
-    ["2005", 1170, 460],
-    ["2006", 660, 1120],
-    ["2007", 1030, 540]
-  ]);
-
-  var options = {
-    title: "Activity",
-    curveType: "function",
-    legend: { position: "bottom" }
-  };
-
-  var chart = new google.visualization.LineChart(
-    document.getElementById("chart")
-  );
-
-  chart.draw(data, options);
-}
 export default {
   name: "Home",
   data() {
-    return {
-      src: "./assets/logo.png"
-    };
+    return {};
+  },
+  mounted() {
+    google.charts.load("current", { packages: ["corechart"] });
+    google.charts.setOnLoadCallback(this.drawChart);
+  },
+  methods: {
+    drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ["Year", "Sales", "Expenses"],
+        ["2004", 1000, 400],
+        ["2005", 1170, 460],
+        ["2006", 660, 1120],
+        ["2007", 1030, 540]
+      ]);
+
+      var options = {
+        title: "Activity",
+        curveType: "function",
+        legend: { position: "bottom" }
+      };
+
+      var chart = new google.visualization.LineChart(
+        document.getElementById("chart")
+      );
+
+      chart.draw(data, options);
+    }
   }
 };
 </script>
-<style scoped>
+<style lang="scss">
+@import "../assets/css/home";
 </style>
---
+++ src/components/product.vue
@@ -0,0 +1,299 @@
+<template>
+    <div id="main">
+        <div class="container">
+            <div class="info">
+                <div class="left">
+                    <label for="" class="checkbox">
+                        <input type="checkbox">
+                        <span><i class="fas fa-check"></i></span>
+                    </label>
+                </div>
+                <div class="right">
+                    <a id="add">ADD NEW PRODUCT<i class="fas fa-plus"></i></a>
+                </div>
+            </div>
+            <table class="table-list">
+                <thead>
+                    <tr>
+                        <th>Product</th>
+                        <th width='100'>Original</th>
+                        <th width='100'>Discount</th>
+                        <th width='70'>Size</th>
+                        <th width='80'>Color</th>
+                        <th width='100'>Inventory</th>
+                        <th width='150'>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="product">
+                            <label for="" class="checkbox">
+                                <input type="checkbox">
+                                <span><i class="fas fa-check"></i></span>
+                            </label>
+                            <div class="pic">
+                                <img :src="require(`@/assets/logo.png`)" alt="">
+                            </div>
+                            <span>Mauris non tem.</span>
+                        </td>
+                        <td><span>$3,200</span></td>
+                        <td><span>$2,800</span></td>
+                        <td colspan="3" class="colspan-size-color-inventory">
+                            <div class="item">
+                                <div class="size"><span>L</span></div>
+                                <div class="color">
+                                    <span>Gray</span>
+                                    <span>Black</span>
+                                </div>
+                                <div class="inventory">
+                                    <span>15</span>
+                                    <span>20</span>
+                                </div>
+                            </div>
+                             <div class="item">
+                                 <div class="size"><span>M</span></div>
+                                <div class="color">
+                                    <span>Gray</span>
+                                    <span>Black</span>
+                                </div>
+                                <div class="inventory">
+                                    <span>15</span>
+                                    <span>20</span>
+                                </div>
+                            </div>
+                             <div class="item">
+                                 <div class="size"><span>S</span></div>
+                                <div class="color">
+                                    <span>Gray</span>
+                                    <span>Black</span>
+                                </div>
+                                <div class="inventory">
+                                    <span>15</span>
+                                    <span>20</span>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="status">
+                            <div class='select'>
+                                <span class="b-yellow">SHIPPING
+                                    <i class="fas fa-caret-down"></i>
+                                </span>
+                                <div class="list">
+                                    <span>Paid</span>
+                                    <span>Unpaid</span>
+                                    <span>Shipping</span>
+                                    <span>Done</span>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td class="product">
+                            <label for="" class="checkbox">
+                                <input type="checkbox">
+                                <span><i class="fas fa-check"></i></span>
+                            </label>
+                            <div class="pic">
+                                <img :src="require(`@/assets/logo.png`)" alt="">
+                            </div>
+                            <span>Mauris non tem.</span>
+                        </td>
+                        <td><span>$3,200</span></td>
+                        <td><span>$2,800</span></td>
+                        <td colspan="3" class="colspan-size-color-inventory">
+                            <div class="item">
+                                <div class="size"><span>L</span></div>
+                                <div class="color">
+                                    <span>Gray</span>
+                                    <span>Black</span>
+                                </div>
+                                <div class="inventory">
+                                    <span>15</span>
+                                    <span>20</span>
+                                </div>
+                            </div>
+                             <div class="item">
+                                 <div class="size"><span>M</span></div>
+                                <div class="color">
+                                    <span>Gray</span>
+                                    <span>Black</span>
+                                </div>
+                                <div class="inventory">
+                                    <span>15</span>
+                                    <span>20</span>
+                                </div>
+                            </div>
+                             <div class="item">
+                                 <div class="size"><span>S</span></div>
+                                <div class="color">
+                                    <span>Gray</span>
+                                    <span>Black</span>
+                                </div>
+                                <div class="inventory">
+                                    <span>15</span>
+                                    <span>20</span>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="status">
+                            <div class='select'>
+                                <span class="b-yellow">SHIPPING
+                                    <i class="fas fa-caret-down"></i>
+                                </span>
+                                <div class="list">
+                                    <span>Paid</span>
+                                    <span>Unpaid</span>
+                                    <span>Shipping</span>
+                                    <span>Done</span>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td class="product">
+                            <label for="" class="checkbox">
+                                <input type="checkbox">
+                                <span><i class="fas fa-check"></i></span>
+                            </label>
+                            <div class="pic">
+                                <img :src="require(`@/assets/logo.png`)" alt="">
+                            </div>
+                            <span>Mauris non tem.</span>
+                        </td>
+                        <td><span>$3,200</span></td>
+                        <td><span>$2,800</span></td>
+                        <td colspan="3" class="colspan-size-color-inventory">
+                            <div class="item">
+                                <div class="size"><span>L</span></div>
+                                <div class="color">
+                                    <span>Gray</span>
+                                    <span>Black</span>
+                                </div>
+                                <div class="inventory">
+                                    <span>15</span>
+                                    <span>20</span>
+                                </div>
+                            </div>
+                             <div class="item">
+                                 <div class="size"><span>M</span></div>
+                                <div class="color">
+                                    <span>Gray</span>
+                                    <span>Black</span>
+                                </div>
+                                <div class="inventory">
+                                    <span>15</span>
+                                    <span>20</span>
+                                </div>
+                            </div>
+                             <div class="item">
+                                 <div class="size"><span>S</span></div>
+                                <div class="color">
+                                    <span>Gray</span>
+                                    <span>Black</span>
+                                </div>
+                                <div class="inventory">
+                                    <span>15</span>
+                                    <span>20</span>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="status">
+                            <div class='select'>
+                                <span class="b-yellow">SHIPPING
+                                    <i class="fas fa-caret-down"></i>
+                                </span>
+                                <div class="list">
+                                    <span>Paid</span>
+                                    <span>Unpaid</span>
+                                    <span>Shipping</span>
+                                    <span>Done</span>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    <div id="box">
+        <div class="content">
+            <div class="title">
+                ADD NEW PRODUCT
+                <i class="fas fa-times"></i>
+            </div>
+            <div class="main">
+                <div class="left">
+                    <div class="fas fa-upload upload">
+                        <b>Drag an image or click here to upload…</b>
+                        <div class="imgs">
+                            <div class="_ic"><img :src="require(`@/assets/logo.png`)"></div>
+                            <div class="_ic"><img :src="require(`@/assets/logo.png`)"></div>
+                            <div class="_ic"><img :src="require(`@/assets/logo.png`)"></div>
+                            <div class="_ic"><img :src="require(`@/assets/logo.png`)"></div>
+                            <div class="_ic"><img :src="require(`@/assets/logo.png`)"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="right">
+                    <b>Product Discription</b>
+                    <input value="Vestibulum nec">
+                    <textarea name="" id="" cols="30" rows="10">Curabitur lobortis id lorem id bibendum. Ut id consectetur magna. Quisque volutpat augue enim, pulvinar lobortis nibh lacinia at. Vestibulum nec erat ut mi sollicitudin porttitor id sit amet risus. Nam tempus vel odio vitae aliquam. </textarea>
+                    <b>Price</b>
+                    <div class="n2">
+                        <div class="set">
+                            <span>Original</span>
+                            <input value="$3,200">
+                        </div>
+                        <div class="set">
+                            <span>Discounts</span>
+                            <input value="$2,800">
+                        </div>
+                    </div>
+                    <b>Specification</b>
+                    <div class="n3">
+                         <div class="set">
+                            <span>Size</span>
+                            <select>
+                                <option value="L">L</option>
+                            </select>
+                        </div>
+                        <div class="set">
+                            <span>Gray</span>
+                            <input value="Gray">
+                        </div>
+                          <div class="set">
+                            <span>Inventory</span>
+                            <input value="$15">
+                        </div>
+                    </div>
+                    <a class="add" @click="boxAdd($event)">ADD NEW SPECIFICATION<i class="fas fa-plus"></i></a>
+                </div>
+                <div class="bottom">
+                    <button>PUBLISH</button>
+                    <a href="">SAVE DRAFT</a>
+                </div>
+            </div>
+        </div>
+        <div class="cover"></div>
+    </div>
+
+    </div>
+</template>
+<script>
+export default {
+  name: "Product",
+  methods: {
+    boxAdd(event) {
+        console.log(event);
+        alert();
+        var $tmp = $(event).prev().clone();
+        console.log($tmp);
+
+        $tmp.insertBefort($(event));
+    }
+  }
+};
+</script>
+<style lang="scss">
+@import "../assets/css/product";
+@import "../assets/css/box";
+</style>
